ec3aec3d7d8e48dd2d89d1d3454c38161686a519
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2022-08-17 14:30:03 +0200

    feat: indentation

--- EventHandler.js
+++ EventHandler.js
@@ -10,10 +10,20 @@ var summary;
 //constructor
 function EventHandler(arrayOfObjects){
     this.arrayOfObjects = arrayOfObjects;
-    this.getEventsBetweenDates = function(start, end){alert("getEventsBetweenDates")}
-    this.getByMonth = function(month){alert("getByMonth")}
-    this.getUniqueDateAndSort = function(){alert("getUniqueDateAndSort")}
-    this.getSummary = function(){alert("getSummary")}
+    this.getEventsBetweenDates = function(start, end){
+        // alert("getEventsBetweenDates");
+        
+        console.log()
+    }
+    this.getByMonth = function(month){
+        alert("getByMonth");
+    }
+    this.getUniqueDateAndSort = function(){
+        alert("getUniqueDateAndSort");
+    }
+    this.getSummary = function(){
+        alert("getSummary");
+    }
 }
 
 // TESTING 
@@ -22,3 +32,4 @@ arrayOfObjects = [
     {name: 'Hiking trip', description: 'Hiking trip with a bunch of University friends', dateStart: '2022/02/14', dateEnd: '2022/02/16'}
 ]
 var newArrayOfObjects = new EventHandler(arrayOfObjects);
+
