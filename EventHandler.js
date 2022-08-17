8eb198ec153424ff49ac008f49bd8d639cb29eaf
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2022-08-17 14:25:51 +0200

    feat: create a constructor

--- EventHandler.js
+++ EventHandler.js
@@ -6,3 +6,13 @@ var getEventsBetweenDates;
 var eventsBygetByMonthMonth;
 var eventsByUniqueDateAndSort;
 var summary;
+
+//constructor
+function EventHandler(arrayOfObjects){
+    this.arrayOfObjects = arrayOfObjects;
+    this.getEventsBetweenDates = function(start, end){alert("getEventsBetweenDates")}
+    this.getByMonth = function(month){alert("getByMonth")}
+    this.getUniqueDateAndSort = function(){alert("getUniqueDateAndSort")}
+    this.getSummary = function(){alert("getSummary")}
+
+}
