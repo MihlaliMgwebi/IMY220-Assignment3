d57dd14a09c25c731be675b7342106288c5234fb
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2022-08-17 14:26:13 +0200

    feat: create an instance of an object

--- EventHandler.js
+++ EventHandler.js
@@ -14,5 +14,11 @@ function EventHandler(arrayOfObjects){
     this.getByMonth = function(month){alert("getByMonth")}
     this.getUniqueDateAndSort = function(){alert("getUniqueDateAndSort")}
     this.getSummary = function(){alert("getSummary")}
-
 }
+
+// TESTING 
+arrayOfObjects = [
+    {name: 'University expo', description: 'Expo to showcase University degrees', dateStart: '2022/02/01', dateEnd: '2022/02/14'},
+    {name: 'Hiking trip', description: 'Hiking trip with a bunch of University friends', dateStart: '2022/02/14', dateEnd: '2022/02/16'}
+]
+var newArrayOfObjects = new EventHandler(arrayOfObjects);
