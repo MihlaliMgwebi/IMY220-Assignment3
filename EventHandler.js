d8b2795dd56c55038d11cf7f088c892cd3d4dfa7
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2022-08-18 20:32:27 +0200

    bug: getUniqueDateAndSort does not reduce as per spec

--- EventHandler.js
+++ EventHandler.js
@@ -42,39 +42,79 @@ function EventHandler(arrayOfObjects){
     }
 
     this.getUniqueDateAndSort = function(){
-        // Double check understanding on dates
+        // Double check understanding 
         console.log("getUniqueDateAndSort:");
-        const eventsByUniqueDateAndSort = arrayOfObjects.reduce((reducedArray, current) => {
-        const x = reducedArray.find(item => ((item.dateStart === current.dateStart) || (item.dateEnd === current.dateEnd)) );
-        if (!x) {
-            return reducedArray.concat([current]);
-        } else {
-            return reducedArray;
-        }
-        }, []);
-        console.log(eventsByUniqueDateAndSort);
-        console.log(" ");
+        var eventsByUniqueDateAndSort = [];
+        // eventsByUniqueDateAndSort = arrayOfObjects.reduce((previousValue, currentValue) => {
+        // //     console.log(reducedArray)
+        // //     var x = reducedArray.find(item => ((item.dateStart === current.dateStart) || (item.dateEnd === current.dateEnd)));
+        // //     if (!x) {
+        // //         reducedArray.concat([current]);
+        // //         return ;
+        // //     } else {
+        // //         return reducedArray;
+        //     // }
+        // }, []);
+
+        // const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+        // const countedNames = names.reduce((allNames, name) => {
+        //     console.log("names: " + allNames[name]);
+        //     allNames[name] ??= 0;
+        //     // //allNames[name]++;
+        //     // Remember to return the object, or the next iteration
+        //     // will receive undefined
+        //     return allNames;
+        // }, {});
+        // countedNames is:
+        // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#remove_duplicate_items_in_an_array
+        const myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
+        // const myArrayWithNoDuplicates = myArray.reduce((previousValue, currentValue) => {
+        //     if (previousValue.indexOf(currentValue) === -1) {
+        //         previousValue.push(currentValue);
+        //     }
+        //     return previousValue;
+        // }, []);
+        // console.log(myArrayWithNoDuplicates);
+
+        eventsByUniqueDateAndSort.sort();
+        eventsByUniqueDateAndSort.reverse();
+        return eventsByUniqueDateAndSort;
     }
 
     this.getSummary = function(optionalParameter){ //optionalParameter
+        //contants? Concatenating return values? Cleaner code.
+        //How to check if array of strings
         //object  //if object or if array
+        // functions to avoid redundancy?
         //if same date
         //if diff dates
         console.log("getSummary:");
         var arrayOfStrings = []; //return value
         var eventsSummary = (optionalParameter === undefined) ? this.arrayOfObjects : optionalParameter;
-       
-        const result = Array.isArray(eventsSummary); // object or array?
+        var result = Array.isArray(eventsSummary); // object or array?
 
-        if(result) {//Array
-            console.log(`[${eventsSummary}] is an array.`);
+        if(result) {//Array of Objects
+            var arrayOfStrings = eventsSummary.filter(function(object){//filter through each object
+                var parsedDateStart = Date.parse(object.dateStart);
+                var parsedDateEnd = Date.parse(object.dateEnd);
+    
+                if (parsedDateEnd == parsedEnd){ //if same day, On [dateStart]: [name] ( [description] )
+                    return ("On " + eventsSummary.dateStart + ": " + eventsSummary.name + " ( " + eventsSummary.description + " )");
+                } else { //range of days, From [dateStart] to [dateEnd]: [name] ( [description] )
+                    return ("From " + eventsSummary.dateStart + " to " + eventsSummary.dateEnd + ": " + eventsSummary.name + " ( " + eventsSummary.description + " )");
+                }
+            });
         }
-        else {//Object
+        else {//Single Object 
             var parsedDateStart = Date.parse(eventsSummary.dateStart);
             var parsedDateEnd = Date.parse(eventsSummary.dateEnd);
-            if (parsedDateStart ==  parsedDateEnd){//if same day
-                    arrayOfStrings.push("On ")
-            }   
+            if (parsedDateStart ==  parsedDateEnd){//if same day, On [dateStart]: [name] ( [description] )
+                    arrayOfStrings.push("On " + eventsSummary.dateStart + ": " + eventsSummary.name + " ( " + eventsSummary.description + " )");
+            } else { //range of days, From [dateStart] to [dateEnd]: [name] ( [description] )
+                return ("From " + eventsSummary.dateStart + " to " + eventsSummary.dateEnd + ": " + eventsSummary.name + " ( " + eventsSummary.description + " )");
+            }
         }
         return arrayOfStrings;
     }
@@ -88,9 +128,15 @@ arrayOfObjects = [
     {name: 'Hiking trip', description: 'Hiking trip with a bunch of University friends', dateStart: '2022/02/14', dateEnd: '2022/02/16'},
     {name: 'Music festival', description: 'Weekend long music festival with a ton of artists performing', dateStart: '2022/05/13', dateEnd: '2022/05/15'}
 ]
+
 var newArrayOfObjects = new EventHandler(arrayOfObjects);
 // newArrayOfObjects.getEventsBetweenDates("2022/02/01", "2022/02/16");
 // newArrayOfObjects.getByMonth(05);
-// newArrayOfObjects.getByiqueDateAndSort();
-//console.log(newArrayOfObjects.getSummary());
-console.log(newArrayOfObjects.getSummary({name: 'Pizza party', description: "Pizza party at work", dateStart: '2022/07/10', dateEnd: '2022/07/10'}));
+console.log(newArrayOfObjects.getUniqueDateAndSort());
+
+
+//getSummary:
+// console.log(newArrayOfObjects.getSummary());
+//console.log(newArrayOfObjects.getSummary({name: 'Market', description: "Farmer's market day long event", dateStart: '2022/06/12', dateEnd: '2022/06/12'}));
+//console.log(newArrayOfObjects.getSummary({name: 'University expo', description: 'Expo to showcase University degrees', dateStart: '2022/02/01', dateEnd: '2022/02/14'}));
+//console.log(newArrayOfObjects.getSummary({name: 'Pizza party', description: "Pizza party at work", dateStart: '2022/07/10', dateEnd: '2022/07/10'}));
