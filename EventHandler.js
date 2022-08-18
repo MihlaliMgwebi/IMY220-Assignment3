f22df805feed945d802632fd7485c89cd01a85f7
MihlaliMgwebi <68788381+MihlaliMgwebi@users.noreply.github.com>
2022-08-18 21:16:21 +0200

    fix: getUniqueDateAndSort returns an array

--- EventHandler.js
+++ EventHandler.js
@@ -41,55 +41,65 @@ function EventHandler(arrayOfObjects){
         console.log(" ");
     }
 
+    // this.getUniqueDateAndSort = function(){
+    //     // Double check understanding 
+    //     console.log("getUniqueDateAndSort:");
+    //     var eventsByUniqueDateAndSort = [];
+    //     // eventsByUniqueDateAndSort = arrayOfObjects.reduce((previousValue, currentValue) => {
+    //     // //     console.log(reducedArray)
+    //     // //     var x = reducedArray.find(item => ((item.dateStart === current.dateStart) || (item.dateEnd === current.dateEnd)));
+    //     // //     if (!x) {
+    //     // //         reducedArray.concat([current]);
+    //     // //         return ;
+    //     // //     } else {
+    //     // //         return reducedArray;
+    //     //     // }
+    //     // }, []);
+
+    //     // const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+    //     // const countedNames = names.reduce((allNames, name) => {
+    //     //     console.log("names: " + allNames[name]);
+    //     //     allNames[name] ??= 0;
+    //     //     // //allNames[name]++;
+    //     //     // Remember to return the object, or the next iteration
+    //     //     // will receive undefined
+    //     //     return allNames;
+    //     // }, {});
+    //     // countedNames is:
+    //     // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+    //     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#remove_duplicate_items_in_an_array
+    //     const myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
+    //     // const myArrayWithNoDuplicates = myArray.reduce((previousValue, currentValue) => {
+    //     //     if (previousValue.indexOf(currentValue) === -1) {
+    //     //         previousValue.push(currentValue);
+    //     //     }
+    //     //     return previousValue;
+    //     // }, []);
+    //     // console.log(myArrayWithNoDuplicates);
+
+    //     eventsByUniqueDateAndSort.sort();
+    //     eventsByUniqueDateAndSort.reverse();
+    //     return eventsByUniqueDateAndSort;
+    // }
+  
     this.getUniqueDateAndSort = function(){
-        // Double check understanding 
-        console.log("getUniqueDateAndSort:");
-        var eventsByUniqueDateAndSort = [];
-        // eventsByUniqueDateAndSort = arrayOfObjects.reduce((previousValue, currentValue) => {
-        // //     console.log(reducedArray)
-        // //     var x = reducedArray.find(item => ((item.dateStart === current.dateStart) || (item.dateEnd === current.dateEnd)));
-        // //     if (!x) {
-        // //         reducedArray.concat([current]);
-        // //         return ;
-        // //     } else {
-        // //         return reducedArray;
-        //     // }
-        // }, []);
-
-        // const names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
-        // const countedNames = names.reduce((allNames, name) => {
-        //     console.log("names: " + allNames[name]);
-        //     allNames[name] ??= 0;
-        //     // //allNames[name]++;
-        //     // Remember to return the object, or the next iteration
-        //     // will receive undefined
-        //     return allNames;
-        // }, {});
-        // countedNames is:
-        // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
-
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#remove_duplicate_items_in_an_array
-        const myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
-        // const myArrayWithNoDuplicates = myArray.reduce((previousValue, currentValue) => {
-        //     if (previousValue.indexOf(currentValue) === -1) {
-        //         previousValue.push(currentValue);
-        //     }
-        //     return previousValue;
-        // }, []);
-        // console.log(myArrayWithNoDuplicates);
-
-        eventsByUniqueDateAndSort.sort();
-        eventsByUniqueDateAndSort.reverse();
-        return eventsByUniqueDateAndSort;
+        var answer = arrayOfObjects.reduce((previousEvent,nextEvent) => {
+            if (typeof previousEvent !== 'object'){
+                console.log(typeof previousEvent); 
+                if (arrayOfObjects.find(element => element.dateStart == nextEvent.dateStart && element.dateEnd == nextEvent.dateEnd && element.name != nextEvent.name)){
+                        return previousEvent + nextEvent;
+                }
+            }
+        });
+        return answer;
     }
-
+    
     this.getSummary = function(optionalParameter){ //optionalParameter
         //contants? Concatenating return values? Cleaner code.
         //How to check if array of strings
         //object  //if object or if array
         // functions to avoid redundancy?
-        //if same date
-        //if diff dates
         console.log("getSummary:");
         var arrayOfStrings = []; //return value
         var eventsSummary = (optionalParameter === undefined) ? this.arrayOfObjects : optionalParameter;
@@ -124,9 +134,11 @@ function EventHandler(arrayOfObjects){
 // Ask about test cases
 arrayOfObjects = [
     {name: 'University expo', description: 'Expo to showcase University degrees', dateStart: '2022/02/01', dateEnd: '2022/02/14'},
-    {name: 'Duplicate University expo', description: 'Expo to showcase University degrees', dateStart: '2022/02/01', dateEnd: '2022/02/14'},
+    {name: 'Music festival', description: 'Weekend long music festival with a ton of artists performing', dateStart: '2022/05/13', dateEnd: '2022/05/15'},
+    {name: 'Market', description: "Farmer's market day long event", dateStart: '2022/06/12', dateEnd: '2022/06/12'},
+    {name: 'Science Expo', description: 'Science expo with sciency stuff', dateStart: '2022/06/12', dateEnd: '2022/06/21'},
     {name: 'Hiking trip', description: 'Hiking trip with a bunch of University friends', dateStart: '2022/02/14', dateEnd: '2022/02/16'},
-    {name: 'Music festival', description: 'Weekend long music festival with a ton of artists performing', dateStart: '2022/05/13', dateEnd: '2022/05/15'}
+    {name: 'Park Picnic', description: 'Picnic event in the park', dateStart: '2022/06/12', dateEnd: '2022/06/12'}
 ]
 
 var newArrayOfObjects = new EventHandler(arrayOfObjects);
